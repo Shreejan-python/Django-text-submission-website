330840d5d68ff9f35a0f2b961ebcbae89b456408
Shreejan <dolaishreejan@gmail.com>
2021-02-16 21:02:59 +0530

    initial commit

---
+++ home/views.py
@@ -0,0 +1,7 @@
+# This is created by me
+
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'index.html')
---
+++ templates/index.js
@@ -0,0 +1,7 @@
+let i =32
+let j = 34
+
+console.log(i + j)
+
+let str = "Shreejan"
+console.log(str, typeof(str))
